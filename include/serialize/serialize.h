8a33dde94b444d9dbcb4e12a1d72d836ddbef006
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2022-08-20 17:28:43 -0700

    Moved get_repr_type inside detail/ directory

---
+++ include/serialize/serialize.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <serialize/detail/aggregate_arity.h>
+#include <serialize/detail/get_repr_type.h>
+#include <serialize/detail/struct_nth_field.h>
+#include <serialize/detail/type.h>
--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,7 @@
 #include <boost/pfr.hpp>
 #include <cstdint>
 #include <cstring>
-#include <serialize/detail/struct_nth_field.h>
-#include <serialize/detail/type.h>
+#include <serialize/serialize.h>
 #include <string>
 #include <tuple>
 #include <type_traits>
@@ -12,110 +11,6 @@
 #include <vector>
 
 namespace detail {
-
-template <bool attempt_compression = true>
-type get_repr_type(const uint64_t &input) {
-
-  if constexpr (!attempt_compression) {
-    return type::uint64;
-  }
-
-  if (input <= std::numeric_limits<uint8_t>::max() &&
-      input >= std::numeric_limits<uint8_t>::min()) {
-    return type::uint64_as_uint8;
-  } else if (input <= std::numeric_limits<uint16_t>::max() &&
-             input >= std::numeric_limits<uint16_t>::min()) {
-    return type::uint64_as_uint16;
-  } else if (input <= std::numeric_limits<uint32_t>::max() &&
-             input >= std::numeric_limits<uint32_t>::min()) {
-    return type::uint64_as_uint32;
-  } else {
-    return type::uint64;
-  }
-}
-
-template <bool attempt_compression = true>
-type get_repr_type(const uint32_t &input) {
-
-  if constexpr (!attempt_compression) {
-    return type::uint32;
-  }
-
-  if (input <= std::numeric_limits<uint8_t>::max() &&
-      input >= std::numeric_limits<uint8_t>::min()) {
-    return type::uint32_as_uint8;
-  } else if (input <= std::numeric_limits<uint16_t>::max() &&
-             input >= std::numeric_limits<uint16_t>::min()) {
-    return type::uint32_as_uint16;
-  } else {
-    return type::uint32;
-  }
-}
-
-template <bool attempt_compression = true>
-type get_repr_type(const uint16_t &input) {
-  if constexpr (!attempt_compression) {
-    return type::uint16;
-  }
-
-  if (input <= std::numeric_limits<uint8_t>::max() &&
-      input >= std::numeric_limits<uint8_t>::min()) {
-    return type::uint16_as_uint8;
-  } else {
-    return type::uint16;
-  }
-}
-
-template <bool attempt_compression = true>
-type get_repr_type(const int64_t &input) {
-  if constexpr (!attempt_compression) {
-    return type::int64;
-  }
-
-  if (input <= std::numeric_limits<int8_t>::max() &&
-      input >= std::numeric_limits<int8_t>::min()) {
-    return type::int64_as_int8;
-  } else if (input <= std::numeric_limits<int16_t>::max() &&
-             input >= std::numeric_limits<int16_t>::min()) {
-    return type::int64_as_int16;
-  } else if (input <= std::numeric_limits<int32_t>::max() &&
-             input >= std::numeric_limits<int32_t>::min()) {
-    return type::int64_as_int32;
-  } else {
-    return type::int64;
-  }
-}
-
-template <bool attempt_compression = true>
-type get_repr_type(const int32_t &input) {
-  if constexpr (!attempt_compression) {
-    return type::int32;
-  }
-
-  if (input <= std::numeric_limits<int8_t>::max() &&
-      input >= std::numeric_limits<int8_t>::min()) {
-    return type::int32_as_int8;
-  } else if (input <= std::numeric_limits<int16_t>::max() &&
-             input >= std::numeric_limits<int16_t>::min()) {
-    return type::int32_as_int16;
-  } else {
-    return type::int32;
-  }
-}
-
-template <bool attempt_compression = true>
-type get_repr_type(const int16_t &input) {
-  if constexpr (!attempt_compression) {
-    return type::int16;
-  }
-
-  if (input <= std::numeric_limits<int8_t>::max() &&
-      input >= std::numeric_limits<int8_t>::min()) {
-    return type::int16_as_int8;
-  } else {
-    return type::int16;
-  }
-}
 
 template <typename T, typename U> void append(const T &value, U &bytes) {
   std::copy(static_cast<const char *>(static_cast<const void *>(&value)),
