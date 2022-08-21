809d9cbb1219cfafa41284e342ca6a40e0ea3545
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2022-08-21 09:42:17 -0700

    Added unit test for nested strutcs

---
+++ test/test_serialize_nested_struct.cpp
@@ -0,0 +1,33 @@
+#include <doctest.hpp>
+#include <serialize/serialize.h>
+
+using doctest::test_suite;
+
+TEST_CASE("Serialize nested struct (1 level)" * test_suite("struct")) {
+  struct my_struct {
+    int value;
+
+    struct nested_struct {
+      float value;
+    };
+    nested_struct nested;
+  };
+
+  my_struct s;
+  s.value = 5;
+  s.nested.value = 3.14f;
+
+  auto bytes = serialize(s);
+  REQUIRE(bytes.size() == 8);
+
+  REQUIRE(bytes[0] == static_cast<uint8_t>(detail::type::int32_as_int8));
+  REQUIRE(bytes[1] == static_cast<uint8_t>(5));
+
+  REQUIRE(bytes[2] == static_cast<uint8_t>(detail::type::struct_));
+  REQUIRE(bytes[3] == static_cast<uint8_t>(detail::type::float32));
+  // 3.14f
+  REQUIRE(bytes[4] == static_cast<uint8_t>(0xc3));
+  REQUIRE(bytes[5] == static_cast<uint8_t>(0xf5));
+  REQUIRE(bytes[6] == static_cast<uint8_t>(0x48));
+  REQUIRE(bytes[7] == static_cast<uint8_t>(0x40));
+}
