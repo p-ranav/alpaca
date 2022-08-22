557f0527e4bbe2cb95fc26c9d5d37b6206f53a72
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2022-08-21 21:07:42 -0700

    Removed unused functions

--- include/serialize/detail/get_repr_type.h
+++ include/serialize/detail/get_repr_type.h
@@ -4,59 +4,6 @@
 namespace detail {
 
 template <bool attempt_compression = true>
-type get_repr_type(const uint64_t &input) {
-
-  if constexpr (!attempt_compression) {
-    return type::uint64;
-  }
-
-  if (input <= std::numeric_limits<uint8_t>::max() &&
-      input >= std::numeric_limits<uint8_t>::min()) {
-    return type::uint64_as_uint8;
-  } else if (input <= std::numeric_limits<uint16_t>::max() &&
-             input >= std::numeric_limits<uint16_t>::min()) {
-    return type::uint64_as_uint16;
-  } else if (input <= std::numeric_limits<uint32_t>::max() &&
-             input >= std::numeric_limits<uint32_t>::min()) {
-    return type::uint64_as_uint32;
-  } else {
-    return type::uint64;
-  }
-}
-
-template <bool attempt_compression = true>
-type get_repr_type(const uint32_t &input) {
-
-  if constexpr (!attempt_compression) {
-    return type::uint32;
-  }
-
-  if (input <= std::numeric_limits<uint8_t>::max() &&
-      input >= std::numeric_limits<uint8_t>::min()) {
-    return type::uint32_as_uint8;
-  } else if (input <= std::numeric_limits<uint16_t>::max() &&
-             input >= std::numeric_limits<uint16_t>::min()) {
-    return type::uint32_as_uint16;
-  } else {
-    return type::uint32;
-  }
-}
-
-template <bool attempt_compression = true>
-type get_repr_type(const uint16_t &input) {
-  if constexpr (!attempt_compression) {
-    return type::uint16;
-  }
-
-  if (input <= std::numeric_limits<uint8_t>::max() &&
-      input >= std::numeric_limits<uint8_t>::min()) {
-    return type::uint16_as_uint8;
-  } else {
-    return type::uint16;
-  }
-}
-
-template <bool attempt_compression = true>
 type get_repr_type(const int64_t &input) {
   if constexpr (!attempt_compression) {
     return type::int64;
