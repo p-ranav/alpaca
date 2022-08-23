2f79130bc1982af11cad98415cadb13c7c785840
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2022-08-22 19:52:34 -0700

    Added unit test for deserializing unordered map

--- test/test_deserialize_map.cpp
+++ test/test_deserialize_map.cpp
@@ -1,6 +1,7 @@
 #include <doctest.hpp>
 #include <map>
 #include <serialize/serialize.h>
+#include <unordered_map>
 
 using doctest::test_suite;
 
@@ -22,5 +23,26 @@ TEST_CASE("Deserialize map<char, int>" * test_suite("map")) {
     REQUIRE(result.value.at('x') == 1);
     REQUIRE(result.value.at('y') == 2);
     REQUIRE(result.value.at('z') == 3);
+  }
+}
+
+TEST_CASE("Deserialize unordered_map<string, vector<int>>" *
+          test_suite("map")) {
+  struct my_struct {
+    std::unordered_map<std::string, std::vector<int>> value;
+  };
+
+  std::vector<uint8_t> bytes;
+
+  {
+    my_struct s{{{"time", {0, 1, 2, 3, 4}}, {"x", {5, 10, 15}}}};
+    bytes = serialize(s);
+  }
+
+  {
+    auto result = deserialize<my_struct>(bytes);
+    REQUIRE(result.value.size() == 2);
+    REQUIRE((result.value.at("time") == std::vector<int>{0, 1, 2, 3, 4}));
+    REQUIRE((result.value.at("x") == std::vector<int>{5, 10, 15}));
   }
 }
