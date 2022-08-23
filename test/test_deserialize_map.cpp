c05c5c5de58e8179365bc10ad3ff64f612fe56e9
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2022-08-22 19:58:57 -0700

    Added unit test to deserialize map of maps

--- test/test_deserialize_map.cpp
+++ test/test_deserialize_map.cpp
@@ -46,3 +46,26 @@ TEST_CASE("Deserialize unordered_map<string, vector<int>>" *
     REQUIRE((result.value.at("x") == std::vector<int>{5, 10, 15}));
   }
 }
+
+TEST_CASE("Deserialize unordered_map<string, vector<int>>" *
+          test_suite("map")) {
+  struct my_struct {
+    std::unordered_map<std::string, std::map<int, double>> value;
+  };
+
+  std::vector<uint8_t> bytes;
+
+  {
+    my_struct s{{{"time", {{0, 1.1}, {1, 2.2}}}, {"x", {{3, 4.4}, {4, 5.5}}}}};
+    bytes = serialize(s);
+  }
+
+  {
+    auto result = deserialize<my_struct>(bytes);
+    REQUIRE(result.value.size() == 2);
+    REQUIRE(
+        (result.value.at("time") == std::map<int, double>{{0, 1.1}, {1, 2.2}}));
+    REQUIRE(
+        (result.value.at("x") == std::map<int, double>{{3, 4.4}, {4, 5.5}}));
+  }
+}
