2474bd757f4a32861e09ef0c7d64d8e7677e0431
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2022-08-23 06:42:45 -0700

    Added more unit tests for serializing/deserializing std array with container value_types

--- test/test_deserialize_array.cpp
+++ test/test_deserialize_array.cpp
@@ -41,3 +41,88 @@ TEST_CASE("Deserialize array<array<int, 3>, 3>" * test_suite("array")) {
                                   {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}}));
   }
 }
+
+TEST_CASE("Deserialize array<size_t>" * test_suite("array")) {
+  struct my_struct {
+    std::array<uint64_t, 6> values;
+  };
+
+  std::vector<uint8_t> bytes;
+
+  {
+    my_struct s{{1, 2, 3, 4, 5, 6}};
+    bytes = serialize(s);
+    REQUIRE(bytes.size() == 6);
+  }
+
+  {
+    auto result = deserialize<my_struct>(bytes);
+    REQUIRE((result.values == std::array<uint64_t, 6>{1, 2, 3, 4, 5, 6}));
+  }
+}
+
+TEST_CASE("Deserialize array<int>" * test_suite("array")) {
+  struct my_struct {
+    std::array<int, 6> values;
+  };
+
+  std::vector<uint8_t> bytes;
+
+  {
+    my_struct s{{1, 2, 3, 4, 5, 6}};
+    bytes = serialize(s);
+    REQUIRE(bytes.size() == 6);
+  }
+
+  {
+    auto result = deserialize<my_struct>(bytes);
+    REQUIRE((result.values == std::array<int, 6>{1, 2, 3, 4, 5, 6}));
+  }
+}
+
+TEST_CASE("Deserialize array<vector<char>>" * test_suite("array")) {
+  struct my_struct {
+    std::array<std::vector<char>, 2> values;
+  };
+
+  std::vector<uint8_t> bytes;
+
+  {
+    my_struct s{{{{'a', 'b', 'c'}, {'d', 'e', 'f'}}}};
+    bytes = serialize(s);
+  }
+
+  {
+    auto result = deserialize<my_struct>(bytes);
+    REQUIRE((result.values == std::array<std::vector<char>, 2>{
+                                  {{'a', 'b', 'c'}, {'d', 'e', 'f'}}}));
+  }
+}
+
+TEST_CASE("Deserialize array<nested_struct>" * test_suite("array")) {
+  struct my_struct {
+    std::array<double, 3> values;
+    struct nested_t {
+      int value;
+    };
+    std::array<nested_t, 3> nested_values;
+  };
+
+  std::vector<uint8_t> bytes;
+
+  {
+    my_struct s{{1.23, 4.56, 7.89},
+                {my_struct::nested_t{1}, my_struct::nested_t{2},
+                 my_struct::nested_t{3}}};
+    bytes = serialize(s);
+  }
+
+  {
+    auto result = deserialize<my_struct>(bytes);
+    REQUIRE((result.values == std::array<double, 3>{1.23, 4.56, 7.89}));
+    REQUIRE(result.nested_values.size() == 3);
+    REQUIRE(result.nested_values[0].value == 1);
+    REQUIRE(result.nested_values[1].value == 2);
+    REQUIRE(result.nested_values[2].value == 3);
+  }
+}
--- test/test_serialize_array.cpp
+++ test/test_serialize_array.cpp
@@ -25,187 +25,172 @@ TEST_CASE("Serialize array<char, 3>" * test_suite("array")) {
   REQUIRE(bytes[2] == 'z');
 }
 
-// TEST_CASE("Serialize vector<uint64_t>" * test_suite("array")) {
-//   struct my_struct {
-//     std::vector<uint64_t> value;
-//   };
+TEST_CASE("Serialize array<uint64_t>" * test_suite("array")) {
+  struct my_struct {
+    std::array<uint64_t, 5> value;
+  };
 
-//   my_struct s{{1, 2, 3, 4, 5}};
-//   auto bytes = serialize(s);
-//   REQUIRE(bytes.size() == 6);
-//   // size
-//   REQUIRE(bytes[0] == static_cast<uint8_t>(5));
-//   // values
-//   REQUIRE(bytes[1] == static_cast<uint8_t>(1));
-//   REQUIRE(bytes[2] == static_cast<uint8_t>(2));
-//   REQUIRE(bytes[3] == static_cast<uint8_t>(3));
-//   REQUIRE(bytes[4] == static_cast<uint8_t>(4));
-//   REQUIRE(bytes[5] == static_cast<uint8_t>(5));
-// }
+  my_struct s{{1, 2, 3, 4, 5}};
+  auto bytes = serialize(s);
+  REQUIRE(bytes.size() == 5);
+  // values
+  REQUIRE(bytes[0] == static_cast<uint8_t>(1));
+  REQUIRE(bytes[1] == static_cast<uint8_t>(2));
+  REQUIRE(bytes[2] == static_cast<uint8_t>(3));
+  REQUIRE(bytes[3] == static_cast<uint8_t>(4));
+  REQUIRE(bytes[4] == static_cast<uint8_t>(5));
+}
 
-// TEST_CASE("Serialize vector<int>" * test_suite("array")) {
-//   struct my_struct {
-//     std::vector<int> value;
-//   };
+TEST_CASE("Serialize array<int, 5>" * test_suite("array")) {
+  struct my_struct {
+    std::array<int, 5> value;
+  };
 
-//   my_struct s{{1, 2, 3, 4, 5}};
-//   auto bytes = serialize(s);
-//   REQUIRE(bytes.size() == 6);
-//   // size
-//   REQUIRE(bytes[0] == static_cast<uint8_t>(5));
-//   // values
-//   REQUIRE(bytes[1] == static_cast<uint8_t>(1));
-//   REQUIRE(bytes[2] == static_cast<uint8_t>(2));
-//   REQUIRE(bytes[3] == static_cast<uint8_t>(3));
-//   REQUIRE(bytes[4] == static_cast<uint8_t>(4));
-//   REQUIRE(bytes[5] == static_cast<uint8_t>(5));
-// }
+  my_struct s{{1, 2, 3, 4, 5}};
+  auto bytes = serialize(s);
+  REQUIRE(bytes.size() == 5);
+  // values
+  REQUIRE(bytes[0] == static_cast<uint8_t>(1));
+  REQUIRE(bytes[1] == static_cast<uint8_t>(2));
+  REQUIRE(bytes[2] == static_cast<uint8_t>(3));
+  REQUIRE(bytes[3] == static_cast<uint8_t>(4));
+  REQUIRE(bytes[4] == static_cast<uint8_t>(5));
+}
 
-// TEST_CASE("Serialize vector<float>" * test_suite("array")) {
-//   struct my_struct {
-//     std::vector<float> value;
-//   };
+TEST_CASE("Serialize array<float, 5>" * test_suite("array")) {
+  struct my_struct {
+    std::array<float, 5> value;
+  };
 
-//   my_struct s{{1.1, 2.2, 3.3, 4.4, 5.5}};
-//   auto bytes = serialize(s);
-//   REQUIRE(bytes.size() == 21);
-//   // size
-//   REQUIRE(bytes[0] == static_cast<uint8_t>(5));
-//   // values
-//   float current_value = 1.1;
-//   for (std::size_t i = 1; i < bytes.size();) {
-//     CONSTRUCT_EXPECTED_VALUE(float, current_value);
-//     current_value += 1.1;
-//     for (std::size_t j = 0; j < expected.size(); ++j) {
-//       REQUIRE(bytes[i++] == expected[j]);
-//     }
-//   }
-// }
+  my_struct s{{1.1, 2.2, 3.3, 4.4, 5.5}};
+  auto bytes = serialize(s);
+  REQUIRE(bytes.size() == 20);
+  // values
+  float current_value = 1.1;
+  for (std::size_t i = 0; i < bytes.size();) {
+    CONSTRUCT_EXPECTED_VALUE(float, current_value);
+    current_value += 1.1;
+    for (std::size_t j = 0; j < expected.size(); ++j) {
+      REQUIRE(bytes[i++] == expected[j]);
+    }
+  }
+}
 
-// TEST_CASE("Serialize vector<bool>" * test_suite("array")) {
-//   struct my_struct {
-//     std::vector<bool> value;
-//   };
+TEST_CASE("Serialize array<bool, 5>" * test_suite("array")) {
+  struct my_struct {
+    std::array<bool, 5> value;
+  };
 
-//   my_struct s{{true, false, true, false, true}};
-//   auto bytes = serialize(s);
-//   REQUIRE(bytes.size() == 6);
-//   // size
-//   REQUIRE(bytes[0] == static_cast<uint8_t>(5));
-//   // values
-//   bool current_value = true;
-//   for (std::size_t i = 1; i < bytes.size();) {
-//     CONSTRUCT_EXPECTED_VALUE(bool, current_value);
-//     current_value = !current_value;
-//     for (std::size_t j = 0; j < expected.size(); ++j) {
-//       REQUIRE(bytes[i++] == expected[j]);
-//     }
-//   }
-// }
+  my_struct s{{true, false, true, false, true}};
+  auto bytes = serialize(s);
+  REQUIRE(bytes.size() == 5);
+  // values
+  bool current_value = true;
+  for (std::size_t i = 0; i < bytes.size();) {
+    CONSTRUCT_EXPECTED_VALUE(bool, current_value);
+    current_value = !current_value;
+    for (std::size_t j = 0; j < expected.size(); ++j) {
+      REQUIRE(bytes[i++] == expected[j]);
+    }
+  }
+}
 
-// TEST_CASE("Serialize vector<std::string>" * test_suite("array")) {
-//   struct my_struct {
-//     std::vector<std::string> value;
-//   };
+TEST_CASE("Serialize array<std::string, 5>" * test_suite("array")) {
+  struct my_struct {
+    std::array<std::string, 5> value;
+  };
 
-//   my_struct s{{"a", "b", "c", "d", "e"}};
-//   auto bytes = serialize(s);
-//   REQUIRE(bytes.size() == 11);
-//   // size
-//   REQUIRE(bytes[0] == static_cast<uint8_t>(5));
-//   // values
-//   char current_value = 'a';
-//   for (std::size_t i = 1; i < bytes.size();) {
-//     REQUIRE(bytes[i++] == static_cast<uint8_t>(1));
+  my_struct s{{"a", "b", "c", "d", "e"}};
+  auto bytes = serialize(s);
+  REQUIRE(bytes.size() == 10);
+  // values
+  char current_value = 'a';
+  for (std::size_t i = 0; i < bytes.size();) {
+    REQUIRE(bytes[i++] == static_cast<uint8_t>(1));
 
-//     CONSTRUCT_EXPECTED_VALUE(char, current_value++);
-//     for (std::size_t j = 0; j < expected.size(); ++j) {
-//       REQUIRE(bytes[i++] == expected[j]);
-//     }
-//   }
-// }
+    CONSTRUCT_EXPECTED_VALUE(char, current_value++);
+    for (std::size_t j = 0; j < expected.size(); ++j) {
+      REQUIRE(bytes[i++] == expected[j]);
+    }
+  }
+}
 
-// TEST_CASE("Serialize vector<vector<int>>" * test_suite("array")) {
-//   struct my_struct {
-//     std::vector<std::vector<int>> value;
-//   };
+TEST_CASE("Serialize array<vector<int>, 2>" * test_suite("array")) {
+  struct my_struct {
+    std::array<std::vector<int>, 2> value;
+  };
 
-//   my_struct s{{{1, 2, 3}, {4, 5, 6}}};
-//   auto bytes = serialize(s);
-//   REQUIRE(bytes.size() == 9);
-//   // size
-//   REQUIRE(bytes[0] == static_cast<uint8_t>(2));
+  my_struct s{{{{1, 2, 3}, {4, 5, 6}}}};
+  auto bytes = serialize(s);
+  REQUIRE(bytes.size() == 8);
 
-//   // first sub-vector
-//   // size
-//   REQUIRE(bytes[1] == static_cast<uint8_t>(3));
-//   // values
-//   REQUIRE(bytes[2] == static_cast<uint8_t>(1));
-//   REQUIRE(bytes[3] == static_cast<uint8_t>(2));
-//   REQUIRE(bytes[4] == static_cast<uint8_t>(3));
+  // first sub-vector
+  // size
+  REQUIRE(bytes[0] == static_cast<uint8_t>(3));
+  // values
+  REQUIRE(bytes[1] == static_cast<uint8_t>(1));
+  REQUIRE(bytes[2] == static_cast<uint8_t>(2));
+  REQUIRE(bytes[3] == static_cast<uint8_t>(3));
 
-//   // second sub-vector
-//   // size
-//   REQUIRE(bytes[5] == static_cast<uint8_t>(3));
-//   // values
-//   REQUIRE(bytes[6] == static_cast<uint8_t>(4));
-//   REQUIRE(bytes[7] == static_cast<uint8_t>(5));
-//   REQUIRE(bytes[8] == static_cast<uint8_t>(6));
-// }
+  // second sub-vector
+  // size
+  REQUIRE(bytes[4] == static_cast<uint8_t>(3));
+  // values
+  REQUIRE(bytes[5] == static_cast<uint8_t>(4));
+  REQUIRE(bytes[6] == static_cast<uint8_t>(5));
+  REQUIRE(bytes[7] == static_cast<uint8_t>(6));
+}
 
-// TEST_CASE("Serialize vector<tuple>" * test_suite("array")) {
-//   struct my_struct {
-//     std::vector<std::tuple<bool, int, float, std::string, char>> values;
-//   };
+TEST_CASE("Serialize array<tuple>" * test_suite("array")) {
+  struct my_struct {
+    std::array<std::tuple<bool, int, float, std::string, char>, 2> values;
+  };
 
-//   my_struct s;
-//   s.values.push_back(std::make_tuple(true, 5, 3.14, "Hello", 'a'));
-//   s.values.push_back(std::make_tuple(false, -15, 2.718, "World", 'z'));
-//   auto bytes = serialize(s);
+  my_struct s;
+  s.values[0] = std::make_tuple(true, 5, 3.14, "Hello", 'a');
+  s.values[1] = std::make_tuple(false, -15, 2.718, "World", 'z');
+  auto bytes = serialize(s);
 
-//   REQUIRE(bytes.size() == 27);
-//   // vector of tuple
+  REQUIRE(bytes.size() == 26);
+  // vector of tuple
 
-//   // vector size
-//   REQUIRE(bytes[0] == static_cast<uint8_t>(2));
+  // start array values
 
-//   // start vector values
+  // array[0]
+  REQUIRE(bytes[0] == static_cast<uint8_t>(true));
+  REQUIRE(bytes[1] == static_cast<uint8_t>(5));
+  {
+    CONSTRUCT_EXPECTED_VALUE(float, 3.14f);
+    for (std::size_t i = 0; i < expected.size(); ++i) {
+      REQUIRE(bytes[2 + i] == expected[i]);
+    }
+  }
+  {
+    REQUIRE(bytes[6] == static_cast<uint8_t>(5));
+    REQUIRE(bytes[7] == static_cast<uint8_t>('H'));
+    REQUIRE(bytes[8] == static_cast<uint8_t>('e'));
+    REQUIRE(bytes[9] == static_cast<uint8_t>('l'));
+    REQUIRE(bytes[10] == static_cast<uint8_t>('l'));
+    REQUIRE(bytes[11] == static_cast<uint8_t>('o'));
+  }
+  REQUIRE(bytes[12] == static_cast<uint8_t>('a'));
 
-//   // vector[0]
-//   REQUIRE(bytes[1] == static_cast<uint8_t>(true));
-//   REQUIRE(bytes[2] == static_cast<uint8_t>(5));
-//   {
-//     CONSTRUCT_EXPECTED_VALUE(float, 3.14f);
-//     for (std::size_t i = 0; i < expected.size(); ++i) {
-//       REQUIRE(bytes[3 + i] == expected[i]);
-//     }
-//   }
-//   {
-//     REQUIRE(bytes[7] == static_cast<uint8_t>(5));
-//     REQUIRE(bytes[8] == static_cast<uint8_t>('H'));
-//     REQUIRE(bytes[9] == static_cast<uint8_t>('e'));
-//     REQUIRE(bytes[10] == static_cast<uint8_t>('l'));
-//     REQUIRE(bytes[11] == static_cast<uint8_t>('l'));
-//     REQUIRE(bytes[12] == static_cast<uint8_t>('o'));
-//   }
-//   REQUIRE(bytes[13] == static_cast<uint8_t>('a'));
-
-//   // vector[1]
-//   REQUIRE(bytes[14] == static_cast<uint8_t>(false));
-//   REQUIRE(bytes[15] == static_cast<uint8_t>(0b10001111));
-//   {
-//     CONSTRUCT_EXPECTED_VALUE(float, 2.718f);
-//     for (std::size_t i = 0; i < expected.size(); ++i) {
-//       REQUIRE(bytes[16 + i] == expected[i]);
-//     }
-//   }
-//   {
-//     REQUIRE(bytes[20] == static_cast<uint8_t>(5));
-//     REQUIRE(bytes[21] == static_cast<uint8_t>('W'));
-//     REQUIRE(bytes[22] == static_cast<uint8_t>('o'));
-//     REQUIRE(bytes[23] == static_cast<uint8_t>('r'));
-//     REQUIRE(bytes[24] == static_cast<uint8_t>('l'));
-//     REQUIRE(bytes[25] == static_cast<uint8_t>('d'));
-//   }
-//   REQUIRE(bytes[26] == static_cast<uint8_t>('z'));
-// }
+  // array[1]
+  REQUIRE(bytes[13] == static_cast<uint8_t>(false));
+  REQUIRE(bytes[14] == static_cast<uint8_t>(0b10001111));
+  {
+    CONSTRUCT_EXPECTED_VALUE(float, 2.718f);
+    for (std::size_t i = 0; i < expected.size(); ++i) {
+      REQUIRE(bytes[15 + i] == expected[i]);
+    }
+  }
+  {
+    REQUIRE(bytes[19] == static_cast<uint8_t>(5));
+    REQUIRE(bytes[20] == static_cast<uint8_t>('W'));
+    REQUIRE(bytes[21] == static_cast<uint8_t>('o'));
+    REQUIRE(bytes[22] == static_cast<uint8_t>('r'));
+    REQUIRE(bytes[23] == static_cast<uint8_t>('l'));
+    REQUIRE(bytes[24] == static_cast<uint8_t>('d'));
+  }
+  REQUIRE(bytes[25] == static_cast<uint8_t>('z'));
+}
