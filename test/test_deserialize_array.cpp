0a1aee0c8a8f4f415d125c0b1e9a670630de7f8f
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2022-08-23 06:29:15 -0700

    Added unit test for multi-dimensional std arrays

--- test/test_deserialize_array.cpp
+++ test/test_deserialize_array.cpp
@@ -21,3 +21,23 @@ TEST_CASE("Deserialize vector<char>" * test_suite("vector")) {
     REQUIRE((result.values == std::array<char, 3>{'x', 'y', 'z'}));
   }
 }
+
+TEST_CASE("Deserialize vector<char>" * test_suite("vector")) {
+  struct my_struct {
+    std::array<std::array<int, 3>, 3> values;
+  };
+
+  std::vector<uint8_t> bytes;
+
+  {
+    my_struct s{{{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}}};
+    bytes = serialize(s);
+    REQUIRE(bytes.size() == 9);
+  }
+
+  {
+    auto result = deserialize<my_struct>(bytes);
+    REQUIRE((result.values == std::array<std::array<int, 3>, 3>{
+                                  {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}}));
+  }
+}
