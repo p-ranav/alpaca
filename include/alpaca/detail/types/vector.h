5de001880488bcf8cd41f2f5696f9404e203e97d
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2022-08-31 14:50:04 -0700

    Reserving count before deserializing vector

--- include/alpaca/detail/types/vector.h
+++ include/alpaca/detail/types/vector.h
@@ -70,6 +70,7 @@ bool from_bytes_to_vector(std::vector<T> &value, const Container &bytes,
   }
 
   // read `size` bytes and save to value
+  value.reserve(size);
   for (std::size_t i = 0; i < size; ++i) {
     T v{};
     from_bytes_router<O>(v, bytes, current_index, end_index, error_code);
