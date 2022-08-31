a928592166e36f07166663022c96fa7b4eb9082b
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2022-08-30 19:38:41 -0700

    Added unit tests for error condition while reading out of array

---
+++ test/test_deserialize_error_version_from_array.cpp
@@ -0,0 +1,79 @@
+#include <alpaca/alpaca.h>
+#include <doctest.hpp>
+using namespace alpaca;
+
+using doctest::test_suite;
+
+TEST_CASE("Deserialize int into another struct { uint16_t }" *
+          test_suite("version")) {
+
+  std::array<uint8_t, 10> bytes;
+
+  {
+    struct my_struct {
+      int value;
+    };
+    my_struct s{5};
+    serialize<my_struct, std::array<uint8_t, 10>, options::with_version>(s, bytes);
+  }
+
+  {
+    struct my_struct {
+      uint16_t value;
+    };
+    std::error_code ec;
+    deserialize<my_struct, std::array<uint8_t, 10>, options::with_version>(bytes, ec);
+    REQUIRE((bool)ec == true);
+    REQUIRE(ec.value() == static_cast<int>(std::errc::invalid_argument));
+  }
+}
+
+TEST_CASE("Deserialize int into struct { int,int,int }" *
+          test_suite("version")) {
+
+  std::array<uint8_t, 10> bytes;
+
+  {
+    struct my_struct {
+      int value;
+    };
+    my_struct s{5};
+    serialize<my_struct, std::array<uint8_t, 10>, options::with_version>(s, bytes);
+  }
+
+  {
+    struct my_struct {
+      int a;
+      int b;
+      int c;
+    };
+    std::error_code ec;
+    deserialize<my_struct, std::array<uint8_t, 10>, options::with_version>(bytes, ec);
+    REQUIRE((bool)ec == true);
+    REQUIRE(ec.value() == static_cast<int>(std::errc::invalid_argument));
+  }
+}
+
+TEST_CASE("Deserialize vector<int> into struct { vector<float> }" *
+          test_suite("version")) {
+
+  std::array<uint8_t, 10> bytes;
+
+  {
+    struct my_struct {
+      std::vector<int> value;
+    };
+    my_struct s{{1, 2, 3, 4, 5}};
+    serialize<my_struct, std::array<uint8_t, 10>, options::with_version>(s, bytes);
+  }
+
+  {
+    struct my_struct {
+      std::vector<float> value;
+    };
+    std::error_code ec;
+    deserialize<my_struct, std::array<uint8_t, 10>, options::with_version>(bytes, ec);
+    REQUIRE((bool)ec == true);
+    REQUIRE(ec.value() == static_cast<int>(std::errc::invalid_argument));
+  }
+}
