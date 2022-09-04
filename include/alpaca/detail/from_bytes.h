37b7ccb3ee3c01148472ff6f96ee85c7f3e2a2fc
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2022-09-04 10:59:29 -0700

    Added from_bytes versions for c-style arrays

--- include/alpaca/detail/from_bytes.h
+++ include/alpaca/detail/from_bytes.h
@@ -46,7 +46,7 @@ from_bytes_crc32(uint32_t &value, Container &bytes, std::size_t &current_index,
 // read as is
 template <options O, typename T, typename Container>
 typename std::enable_if<
-    !std::is_same_v<Container, std::ifstream> &&
+    !std::is_same_v<Container, std::ifstream> && !std::is_array_v<Container> &&
         (std::is_same_v<T, int8_t> || std::is_same_v<T, int16_t> ||
          std::is_same_v<T, uint8_t> || std::is_same_v<T, uint16_t> ||
          std::is_same_v<T, char> || std::is_same_v<T, wchar_t> ||
@@ -88,6 +88,58 @@ from_bytes(T &value, Container &bytes, std::size_t &current_index,
     return false;
   }
   value = *(reinterpret_cast<const T *>(bytes.data() + current_index));
+  current_index += num_bytes_to_read;
+  update_value_based_on_alpaca_endian_rules<O, T>(value);
+  return true;
+}
+
+// char, bool, small ints, float, double
+// read as is
+// c-style array version
+template <options O, typename T, typename Container>
+typename std::enable_if<
+    std::is_array_v<Container> &&
+        (std::is_same_v<T, int8_t> || std::is_same_v<T, int16_t> ||
+         std::is_same_v<T, uint8_t> || std::is_same_v<T, uint16_t> ||
+         std::is_same_v<T, char> || std::is_same_v<T, wchar_t> ||
+         std::is_same_v<T, char16_t> || std::is_same_v<T, char32_t> ||
+         std::is_same_v<T, bool> || std::is_same_v<T, float> ||
+         std::is_same_v<T, double>),
+    bool>::type
+from_bytes(T &value, Container &bytes, std::size_t &current_index,
+           std::size_t &end_index, std::error_code &error_code) {
+
+  if (current_index >= end_index) {
+    // end of input
+
+    // default initialize the value
+    value = T();
+
+    // return true for forward compatibility
+    return true;
+  }
+
+  if constexpr (std::is_same_v<T, uint8_t> || std::is_same_v<T, int8_t>) {
+    // there should be at least 1 byte
+    if (end_index - current_index < 1) {
+      error_code = std::make_error_code(std::errc::message_size);
+      return false;
+    }
+  } else if constexpr (std::is_same_v<T, uint16_t> ||
+                       std::is_same_v<T, int16_t>) {
+    // there should be at least 2 bytes
+    if (end_index - current_index < 2) {
+      error_code = std::make_error_code(std::errc::message_size);
+      return false;
+    }
+  }
+
+  constexpr auto num_bytes_to_read = sizeof(T);
+  if (end_index < num_bytes_to_read) {
+    /// TODO: report error
+    return false;
+  }
+  value = *(reinterpret_cast<const T *>(bytes + current_index));
   current_index += num_bytes_to_read;
   update_value_based_on_alpaca_endian_rules<O, T>(value);
   return true;
@@ -151,7 +203,7 @@ from_bytes(T &value, Container &bytes, std::size_t &current_index,
 // decode variable-length encoding
 template <options O, typename T, typename Container>
 typename std::enable_if<
-    !std::is_same_v<Container, std::ifstream> &&
+    !std::is_same_v<Container, std::ifstream> && !std::is_array_v<Container> &&
         (std::is_same_v<T, int32_t> || std::is_same_v<T, int64_t> ||
          std::is_same_v<T, uint32_t> || std::is_same_v<T, uint64_t> ||
          std::is_same_v<T, std::size_t>),
@@ -187,6 +239,56 @@ from_bytes(T &value, Container &bytes, std::size_t &current_index,
       return false;
     }
     value = *(reinterpret_cast<const T *>(bytes.data() + current_index));
+    current_index += num_bytes_to_read;
+  } else {
+    value = decode_varint<T>(bytes, current_index);
+  }
+
+  update_value_based_on_alpaca_endian_rules<O, T>(value);
+  return true;
+}
+
+// large ints
+// decode variable-length encoding
+// c-style array version
+template <options O, typename T, typename Container>
+typename std::enable_if<
+    std::is_array_v<Container> &&
+        (std::is_same_v<T, int32_t> || std::is_same_v<T, int64_t> ||
+         std::is_same_v<T, uint32_t> || std::is_same_v<T, uint64_t> ||
+         std::is_same_v<T, std::size_t>),
+    bool>::type
+from_bytes(T &value, Container &bytes, std::size_t &current_index,
+           std::size_t &end_index, std::error_code &) {
+
+  if (current_index >= end_index) {
+    // end of input
+
+    // default initialize the value
+    value = T();
+
+    // return true for forward compatibility
+    return true;
+  }
+
+  constexpr auto use_fixed_length_encoding = (
+      // Do not perform VLQ if:
+      // 1. if the system is little-endian and the requested byte order is
+      // big-endian
+      // 2. if the system is big-endian and the requested byte order is
+      // little-endian
+      // 3. If fixed length encoding is requested
+      (is_system_little_endian() && detail::big_endian<O>()) ||
+      (is_system_big_endian() && detail::little_endian<O>()) ||
+      (detail::fixed_length_encoding<O>()));
+
+  if constexpr (use_fixed_length_encoding) {
+    constexpr auto num_bytes_to_read = sizeof(T);
+    if (end_index < num_bytes_to_read) {
+      /// TODO: report error
+      return false;
+    }
+    value = *(reinterpret_cast<const T *>(bytes + current_index));
     current_index += num_bytes_to_read;
   } else {
     value = decode_varint<T>(bytes, current_index);
