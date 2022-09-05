d7c7a93117e75db7b9c40e193d9277c180de83a7
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2022-09-05 08:20:15 -0700

    Updated mesh benchmark test to use array and check result

--- benchmark/alpaca_benchmark.cpp
+++ benchmark/alpaca_benchmark.cpp
@@ -3,7 +3,7 @@
 #include "mesh.h"
 #include <random>
 
-static void BM_alpaca_serialize_mesh_125k(benchmark::State &state) {
+static void BM_alpaca_serialize_mesh_125k_array(benchmark::State &state) {
   {
     std::random_device rd;
     std::default_random_engine eng(rd());
@@ -12,8 +12,7 @@ static void BM_alpaca_serialize_mesh_125k(benchmark::State &state) {
     alpaca::benchmark::Mesh m;
     alpaca::benchmark::generate_mesh<125000>(m, eng, distr);
 
-    std::vector<uint8_t> bytes;
-    bytes.reserve(6000024);
+    std::array<uint8_t, 6000024> bytes;
     std::size_t data_size = 0;
 
     for (auto _ : state) {
@@ -22,10 +21,14 @@ static void BM_alpaca_serialize_mesh_125k(benchmark::State &state) {
       data_size = alpaca::serialize(m, bytes);
     }
 
+    std::error_code ec;
+    auto m_recovered = alpaca::deserialize<alpaca::benchmark::Mesh>(bytes, ec);
+    state.counters["Success"] = ((bool)ec == false);
     state.counters["BytesOutput"] = data_size;
+    state.counters["Triangles"] = m_recovered.triangles.size();
   }
 }
 
-BENCHMARK(BM_alpaca_serialize_mesh_125k);
+BENCHMARK(BM_alpaca_serialize_mesh_125k_array);
 
 BENCHMARK_MAIN();
