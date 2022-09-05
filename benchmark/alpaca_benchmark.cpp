28fec988ce3a7bf5cc615024c6ee712dab3aa4ae
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2022-09-05 08:29:00 -0700

    Added mesh deserialization benchmark

--- benchmark/alpaca_benchmark.cpp
+++ benchmark/alpaca_benchmark.cpp
@@ -3,7 +3,7 @@
 #include "mesh.h"
 #include <random>
 
-static void BM_alpaca_serialize_mesh_125k_array(benchmark::State &state) {
+static void BM_serialize_mesh_125k(benchmark::State &state) {
   {
     std::random_device rd;
     std::default_random_engine eng(rd());
@@ -29,6 +29,33 @@ static void BM_alpaca_serialize_mesh_125k_array(benchmark::State &state) {
   }
 }
 
-BENCHMARK(BM_alpaca_serialize_mesh_125k_array);
+static void BM_deserialize_mesh_125k(benchmark::State &state) {
+  {
+    std::random_device rd;
+    std::default_random_engine eng(rd());
+    std::uniform_real_distribution<float> distr(-1000.0f, 1000.0f);
+
+    alpaca::benchmark::Mesh m;
+    alpaca::benchmark::generate_mesh<125000>(m, eng, distr);
+
+    std::array<uint8_t, 6000024> bytes;
+    auto data_size = alpaca::serialize(m, bytes);
+
+    std::error_code ec;
+    alpaca::benchmark::Mesh m_recovered;
+
+    for (auto _ : state) {
+      // This code gets timed
+      m_recovered = alpaca::deserialize<alpaca::benchmark::Mesh>(bytes, ec);
+    }
+
+    state.counters["Success"] = ((bool)ec == false);
+    state.counters["BytesOutput"] = data_size;
+    state.counters["Triangles"] = m_recovered.triangles.size();
+  }
+}
+
+BENCHMARK(BM_serialize_mesh_125k);
+BENCHMARK(BM_deserialize_mesh_125k);
 
 BENCHMARK_MAIN();
